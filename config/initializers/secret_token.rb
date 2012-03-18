15ce63f0d4eac823b64eeace3f325a409d573830
Susie Marshall <susie2701@yahoo.co.uk>
2012-03-18 09:15:13 -0700

    added new project to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '0c21d1baff1e9f77f602b361ff4b4db08b11ab0e8f3e46493a878076faf4d3444ee0996051c8ea9899171f2bac5e12ab3fd6e97e8f6389f7c4b90fe8f466e900'
